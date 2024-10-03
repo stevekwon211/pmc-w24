bd5c011101eb0c949328ba4b91ed27d8c615844d
stevekwon211 <stevekwon211@gmail.com>
2024-10-03 23:41:17 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,23 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+// Remove or comment out these lines if they're not being used
+// const geistSans = GeistSans;
+// const geistMono = GeistMono;
 
 export const metadata: Metadata = {
     title: "PMC W24",
     description: "4주 동안 프로덕트 만들고 런칭하는 레이스",
-    icons: [{ rel: "icon", url: "/blink-aster.gif", type: "image/gif" }],
+    icons: [
+        { rel: "icon", url: "/blink-aster.gif", type: "image/gif" },
+        { rel: "icon", url: "/blink-aster-img.png", type: "image/png" },
+    ],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="ko" className={`${GeistSans.variable} ${GeistMono.variable}`}>
             <body>{children}</body>
         </html>
     );
