5753b1b75698bc449228fbbebc69a1f1af60226c
stevekwon211 <stevekwon211@gmail.com>
2024-10-08 17:31:56 +0900

    bbb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -12,6 +13,30 @@ const neodunggeunmo = localFont({
     ],
     display: "swap",
 });
+
+export const metadata: Metadata = {
+    title: "PMC W24",
+    description: "4주 동안 프로덕트 만들고 런칭하는 레이스",
+    openGraph: {
+        title: "PMC W24",
+        description: "4주 동안 프로덕트 만들고 런칭하는 레이스",
+        images: [
+            {
+                url: "/large-bg.png", // public 폴더 내의 이미지 경로
+                width: 1200,
+                height: 630,
+                alt: "PMC W24",
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "PMC W24",
+        description: "4주 동안 프로덕트 만들고 런칭하는 레이스",
+        images: ["/og-image.png"], // Twitter도 같은 이미지 사용
+    },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
